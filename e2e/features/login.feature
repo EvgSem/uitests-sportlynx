a0a90994dc73e2a7ce1e5ed3b41bcaa590276b2d
EvgSem <esemindieieva@gmail.com>
2016-09-02 10:26:39 -0700

    added arguments to specs, default values are in conf.js

--- e2e/features/login.feature
+++ e2e/features/login.feature
@@ -1,8 +1,8 @@
 Feature: Log in
 
   Scenario: Enter correct login and password
-    Given I navigate to homepage
-    When  I input email to input-text email
-    And   I input password to input-text password
+    Given I navigate to login page
+    When  I input 'default' to input-text email
+    And   I input 'default' to input-text password
     And   I click on button SingIn
-    Then  I should see welcome title
+    Then  I should see welcome title 'Welcome to SportLynx!'
--- e2e/spec/login-page.spec.js
+++ e2e/spec/login-page.spec.js
@@ -6,20 +6,22 @@ var logInPage = require('./login.page.js');
 var homePage = require('./home.page.js');  
 
 module.exports = function () {
-	this.Given(/^I navigate to homepage$/, function (callback) {
+	this.Given(/^I navigate to login page$/, function (callback) {
 	    browser.get(browser.baseUrl)
 	    .then(callback);
 	});
 
-	this.When(/^I input email to input-text email$/, function (callback) {
+	this.When(/^I input '(.*)' to input-text email$/, function (value, callback) {
 		var loginPageObj = new logInPage();
-		loginPageObj.emailInput.sendKeys(browser.params.login.email)
+		var emailValue =  (value == 'default' ? browser.params.login.email : value);
+		loginPageObj.emailInput.sendKeys(emailValue)
 		.then(callback);
 	});
 
-	this.When(/^I input password to input-text password$/, function (callback) {
+	this.When(/^I input '(.*)' to input-text password$/, function (value, callback) {
 		var loginPageObj = new logInPage();
-		loginPageObj.passwordInput.sendKeys("password")
+		var passwordValue =  (value == 'default' ? browser.params.login.password : value);
+		loginPageObj.passwordInput.sendKeys(passwordValue)
 		.then(callback);
 	});
 
@@ -29,7 +31,7 @@ module.exports = function () {
 		.then(callback);
 	});
 
-	this.Then(/^I should see welcome title$/, function (callback) {
+	this.Then(/^I should see welcome title '(.*)'$/, function (value, callback) {
 		var homePageObj = new homePage();
 		expect(homePageObj.welcomeTitle.getText()).to.eventually.equal('Welcome to SportLynx!')
 	    .then(function(){
